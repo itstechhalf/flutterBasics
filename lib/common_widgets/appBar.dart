be0355eaed3becbbf358e0ab91574775310c9dc8
itsTechhaf <zaintv308@gmail,com>
2024-10-09 20:20:54 +0500

    AppBar Added

---
+++ lib/common_widgets/appBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class appBar {
+   //Basic App Bar
+
+   final myAppBar = AppBar(
+    title: const Text(
+      "Flutter Basics",
+      style: TextStyle(color: Colors.white),
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.blue,
+  );
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbasics/common_widgets/appBar.dart';
 
 class home extends StatelessWidget {
   const home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var basicAppBar = appBar();
+    return Scaffold(
+      appBar:basicAppBar.myAppBar,
+    );
   }
 }
